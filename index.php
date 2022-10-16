d54f7542a5e8c4b9b34cab93ad47d11a2aa90364
Sergio Mejia <sdmejiac@correo.udistrital.edu.co>
2022-10-16 11:37:45 -0500

    Creacion de las ondas highcharts

--- index.php
+++ index.php
@@ -5,62 +5,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 
 <body>
     <form action="./index.php" method="POST">
-        <input type="number" name="frecuencia" id="frecuencia" placeholder="frecuencia">
-        <input type="number" name="BW" id="BW" placeholder="BW">
-        <input type="number" name="PP" id="PP" placeholder="Potencia Pico">
+        <div>
+            <input type="number" name="frecuencia" id="frecuencia" placeholder="frecuencia">
+            <input type="number" name="BW" id="BW" placeholder="BW">
+            <input type="number" name="PP" id="PP" placeholder="Potencia Pico">
+        </div>
+        <div>
+            <input type="number" name="frecuencia2" id="frecuencia2" placeholder="frecuencia">
+            <input type="number" name="BW2" id="BW2" placeholder="BW">
+            <input type="number" name="PP2" id="PP2" placeholder="Potencia Pico">
+        </div>
+        <div>
+            <input type="number" name="frecuencia3" id="frecuencia3" placeholder="frecuencia">
+            <input type="number" name="BW3" id="BW3" placeholder="BW">
+            <input type="number" name="PP3" id="PP3" placeholder="Potencia Pico">
+        </div>
         <input type="submit" name="subir" id="subir" value="Hecho">
     </form>
 
-        <script type='text/javascript'>
-            google.charts.load('current', {
-                'packages': ['line']
-            });
-            google.charts.setOnLoadCallback(drawChart);
-
-            function drawChart() {
-
-                var data = new google.visualization.DataTable();
-                data.addColumn('number', 'Day');
-                data.addColumn('number', 'Guardians of the Galaxy');
-                data.addColumn('number', 'The Avengers');
-                data.addColumn('number', 'Transformers: Age of Extinction');
-
-                data.addRows([
-                    [1, 37.8, 80.8, 41.8],
-                    [2, 30.9, 69.5, 32.4],
-                    [3, 25.4, 57, 25.7],
-                    [4, 11.7, 18.8, 10.5],
-                    [5, 11.9, 17.6, 10.4],
-                    [6, 8.8, 13.6, 7.7],
-                    [7, 7.6, 12.3, 9.6],
-                    [8, 12.3, 29.2, 10.6],
-                    [9, 16.9, 42.9, 14.8],
-                    [10, 12.8, 30.9, 11.6],
-                    [11, 5.3, 7.9, 4.7],
-                    [12, 6.6, 8.4, 5.2],
-                    [13, 4.8, 6.3, 3.6],
-                    [14, 4.2, 6.2, 3.4]
-                ]);
-
-                var options = {
-                    chart: {
-                        title: 'Box Office Earnings in First Two Weeks of Opening',
-                        subtitle: 'in millions of dollars (USD)'
-                    },
-                    width: 900,
-                    height: 500
-                };
-
-                var chart = new google.charts.Line(document.getElementById('linechart_material'));
-
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
-        </script>
         
     <?php
     if (isset($_POST['subir'])) {
@@ -69,54 +37,109 @@
         $PP = $_POST['PP'];
         $BW = $_POST['BW']; 
 
+        $frecuencia2 = $_POST['frecuencia2'];
+        $PP2 = $_POST['PP2'];
+        $BW2 = $_POST['BW2']; 
+
+        $frecuencia3 = $_POST['frecuencia3'];
+        $PP3 = $_POST['PP3'];
+        $BW3 = $_POST['BW3']; 
+
+
+
         $piso_ruido = -100;
 
         echo "
-        <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
-        <script type='text/javascript'>
-            google.charts.load('current', {
-                'packages': ['line']
-            });
-            google.charts.setOnLoadCallback(drawChart);
-
-            function drawChart() {
-
-                var data = new google.visualization.DataTable();
-                data.addColumn('number', 'Frecuencia');
-                data.addColumn('number', 'señal 1');
-                data.addColumn('number', 'señal 2');
-
-
-                data.addRows([
-                    [".($frecuencia-($BW)).", ".$piso_ruido.",".$piso_ruido."],
-                    [".($frecuencia-($BW/2)).", ".($PP-3).",".$piso_ruido."],
-                    [".$frecuencia.", ".$PP.",".$piso_ruido."],
-                    [".($frecuencia+($BW/2)).", ".($PP-3).",".$piso_ruido."],
-                    [".($frecuencia+($BW)).", ".$piso_ruido.",".$piso_ruido."],
-                ]);
-
-                data.addRows([
-                    [70, ".$piso_ruido.",".$piso_ruido."],
-                    [75,".$piso_ruido.",5],
-                ]);
-
-
-
-                var options = {
-                    chart: {
-                        title: 'Visualizacion de señales de radiofrecuencia',
-                        subtitle: 'dBm'
+        <script src='https://code.highcharts.com/highcharts.js'></script>
+        <script src='https://code.highcharts.com/modules/series-label.js'></script>
+        <script src='https://code.highcharts.com/modules/exporting.js'></script>
+        <script src='https://code.highcharts.com/modules/export-data.js'></script>
+        <script src='https://code.highcharts.com/modules/accessibility.js'></script>
+    
+        <figure class='highcharts-figure'>
+            <div id='container'></div>
+            <p class='highcharts-description'>
+                This demo visualizes a data set with irregular time intervals.
+                Highcharts comes with sophisticated functionality for dealing
+                with time data, including support for different time zones and
+                irregular intervals.
+            </p>
+        </figure>
+    
+        <script>
+            // Data retrieved from https://www.vikjavev.no/ver/snjomengd
+    
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'spline'
+                },
+                title: {
+                    text: 'Snow depth at Vikjafjellet, Norway'
+                },
+                subtitle: {
+                    text: 'Irregular time data in Highcharts JS'
+                },
+                xAxis: {
+                    type: 'text',
+                    title: {
+                        text: 'Date'
                     },
-                    width: 900,
-                    height: 500
-                };
+                    min: -150
+                },
+                yAxis: {
+                    title: {
+                        text: 'dBm'
+                    },
+                    min: -150
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+                },
+    
+                plotOptions: {
+                    series: {
+                        marker: {
+                            enabled: true,
+                            radius: 2.5
+                        }
+                    }
+                },
+    
+                colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
 
-                var chart = new google.charts.Line(document.getElementById('linechart_material'));
-
-                chart.draw(data, google.charts.Line.convertOptions(options));
-            }
+                series: [
+                    {
+                        name: 'Señal 1',
+                        data: [
+                            [".($frecuencia-($BW)).", ".$piso_ruido."],
+                            [".($frecuencia-($BW/2)).", ".($PP-3)."],
+                            [".$frecuencia.",".$PP."],
+                            [".($frecuencia+($BW/2)).", ".($PP-3)."],
+                            [".($frecuencia+($BW)).", ".$piso_ruido."],
+                        ]
+                    }, {
+                        name: 'Señal 2',
+                        data: [
+                            [".($frecuencia2-($BW2)).", ".$piso_ruido."],
+                            [".($frecuencia2-($BW2/2)).", ".($PP2-3)."],
+                            [".$frecuencia2.",".$PP2."],
+                            [".($frecuencia2+($BW2/2)).", ".($PP2-3)."],
+                            [".($frecuencia2+($BW2)).", ".$piso_ruido."],
+                        ]
+                    }, {
+                        name: 'Señal 3',
+                        data: [
+                            [".($frecuencia3-($BW3)).", ".$piso_ruido."],
+                            [".($frecuencia3-($BW3/2)).", ".($PP3-3)."],
+                            [".$frecuencia3.",".$PP3."],
+                            [".($frecuencia3+($BW3/2)).", ".($PP3-3)."],
+                            [".($frecuencia3+($BW3)).", ".$piso_ruido."],
+                        ]
+                    }
+                ]
+            });
         </script>
-        <div id='linechart_material' style='width: 900px; height: 500px'></div>
         ";
     }
     ?>
